77afaf07a3b968379292c8cd3f1e62aa4826a05e
DanteCarrizo <dantecarrizoph@gmail.com>
2023-09-14 21:35:29 -0300

    Primer commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'; // Importa los componentes de Bootstrap
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#">Nombre de la Tienda</Navbar.Brand> {/* Título de la tienda */}
+      <Navbar.Brand href="#">Clearmind</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#">Inicio</Nav.Link> {/* Puedes reemplazar "#" con la URL real */}
+          <Nav.Link href="#">Inicio</Nav.Link>
           <NavDropdown title="Categorías" id="basic-nav-dropdown">
             <NavDropdown.Item href="#">Categoría 1</NavDropdown.Item>
             <NavDropdown.Item href="#">Categoría 2</NavDropdown.Item>
